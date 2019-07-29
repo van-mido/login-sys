2376ea0792a56fb62094f35eb3fe0ee565f029ea
Ricardo Vargas <hymura@gmail.com>
2019-07-29 12:28:27 -0500

    File Updated

--- views/logout.view.php
+++ views/logout.view.php
@@ -1,5 +1,12 @@
-<h1>Logout page</h1>
-
-<p>You have successfully logout</p>
-
-<noscript><a href="login.php">Login Page</a></noscript>
+<div class="container">
+	<div class="row">
+		<div class="col-md-9">
+			<h1>Logout page</h1>
+				
+				<div class="alert alert-primary" role="alert">
+					You have successfully logout
+					<noscript><a href="login.php">Login Page</a></noscript>
+				</div>
+		</div>
+	</div>
+</div>
